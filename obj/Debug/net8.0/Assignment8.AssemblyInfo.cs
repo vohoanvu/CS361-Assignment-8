aec82303139387667fe97cb89507ad9940c3c70c
Vu Vo <vohoanvu96@gmail.com>
2024-08-05 23:35:36 +0700

    ignore sensitive file

--- obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c5c6c548a8d1f3ff3a9ba448f01e29271615a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266af510caad0f41c3e1edf66b8aa8e1f9724f8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
