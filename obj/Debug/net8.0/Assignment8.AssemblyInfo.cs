7e5a926ea00aa8a12ba4b8c7c71c6709aa3294da
Vu Vo <vohoanvu96@gmail.com>
2024-08-06 19:09:47 +0700

    going live

--- obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdacd0476e6384e240e57f71181de6660bf40202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422e0e9dd2aa27b2c1637fb08a897dca5c29cc43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
