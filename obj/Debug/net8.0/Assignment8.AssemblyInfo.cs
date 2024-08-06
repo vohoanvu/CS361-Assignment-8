343f7d629bb4a8d0396c3df6fba275429c2224eb
Vu Vo <vohoanvu96@gmail.com>
2024-08-06 18:44:44 +0700

    Release build

--- obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266af510caad0f41c3e1edf66b8aa8e1f9724f8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213745fccbcc251076431336e4cb67570fa61bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
